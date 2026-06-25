2680f06514c7cc1b9955ca6a040fd4ebda3fae4c
imraj071 <imraj071@gmail.com>
2026-06-25 20:12:43 +0530

    renamed userController

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUserProject, getUserCredits, getUSerProject, getUSerProjects, purchaseCredits, togglePublish } from "../controllers/userControllers.js";
+import { createUserProject, getUserCredits, getUSerProject, getUSerProjects, purchaseCredits, togglePublish } from "../controllers/userController.js";
 import { protect } from "../middlewares/auth.js";
 
 const userRouter = express.Router();
