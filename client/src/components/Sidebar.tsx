5c4620a4320126b18c265bc0f141277139b967df
imraj071 <imraj071@gmail.com>
2026-06-26 17:25:05 +0530

    Created handleRollback function in Sidebar component

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -30,7 +30,28 @@ const fetchProjects = async ()=> {
 }
 
 const handleRollBack = async (versionId: string) => {
+    try {
 
+        const confirm =  window.confirm('Are you sure you want to rollback to this version?')
+        if(!confirm){
+            return;
+        }
+
+        setIsGenerating(true)
+        const {data} = await api.get(`/api/project/rollback/${project.id}/${versionId}`)
+        const {data: data2} = await api.get(`/api/user/project/${project.id}`)
+
+        toast.success(data.message)
+
+        setProject(data2.project)
+        setIsGenerating(false)
+        
+
+    } catch (error: any) {
+        setIsGenerating(false)
+        toast.error(error?.response?.data?.message || error.message);
+        console.log(error);
+    }
 }
 
 const handleRevisions = async (e:React.FormEvent) => {
@@ -51,7 +72,7 @@ const handleRevisions = async (e:React.FormEvent) => {
         setIsGenerating(false)
     } catch (error: any) {
         setIsGenerating(false);
-        toast.error(error?.reponse?.data?.message || error.message);
+        toast.error(error?.response?.data?.message || error.message);
         console.log(error);
         clearInterval(interval)
     }
