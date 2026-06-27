20acfa51bb0c3bc34e5ba58406c250294802092e
imraj071 <imraj071@gmail.com>
2026-06-27 15:34:24 +0530

    create handlePurchase Function in Pricing Page

--- client/src/pages/Pricing.tsx
+++ client/src/pages/Pricing.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { appPlans } from "../assets/assets";
 import Footer from "../components/Footer";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
+import api from "@/configs/axios";
 
 
 interface Plan {
@@ -13,10 +16,22 @@ interface Plan {
 }
 const Pricing = () => {
 
+    const {data: session} = authClient.useSession();
+
   const [plans] = useState<Plan[]>(appPlans)
 
   const handlePurchase = async (planId:string) => {
-        
+        try {
+
+            if(!session?.user) return toast('Please login to purchase credits');
+            
+            const {data} = await api.post('/api/user/purchase-credits', {planId})
+            window.location.href = data.payment_link;
+
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || error.message);
+            console.log(error);    
+        }
     }
 
   return (
