f7e1d1add423e2240822bc82aaf884a3c772acbc
imraj071 <imraj071@gmail.com>
2026-06-27 15:37:05 +0530

    fixed purchase-credits bug in userRoutes

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -9,6 +9,6 @@ userRouter.post('/project', protect, createUserProject)
 userRouter.get('/project/:projectId', protect, getUSerProject)
 userRouter.get('/projects', protect, getUSerProjects)
 userRouter.get('/publish-toggle/:projectId', protect, togglePublish)
-userRouter.post('/pruchase-credits', protect, purchaseCredits)
+userRouter.post('/purchase-credits', protect, purchaseCredits)
 
 export default userRouter;
